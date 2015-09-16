148d424e27de5e3148554a42973dd7fffd779082
HB <HB@HB-PC>
2015-09-16 18:03:33 +0900

    2015.09.16 연산자

--- java-algo/src/operator/ArithmeticDemo.java
+++ java-algo/src/operator/ArithmeticDemo.java
@@ -8,15 +8,14 @@ package operator;
 public class ArithmeticDemo {
 	public static void main(String[] args) {
 		
-		int x = 9, y = 5;
-		int z = 0;
+		int x = 9, y = 5, z = 0;
 		z = x / y;
-		System.out.println("x / y = "+z);
+		System.out.println("몫만 취하는 x / y = "+z);
 		// 프로그래밍에서 나눗셈은 두가지 결과값을 가진다
 		// 하나는 몫만 취하는 것이고, 다른 하나는 나머지 값만 취한다.
 		
 		z = x % y;
-		System.out.println("x % y = "+y);
-				
+		System.out.println("나머지 값만 취하는 x % y = "+z);
+
 	}
 }
